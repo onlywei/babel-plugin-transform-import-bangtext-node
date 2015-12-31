15ebc447a15dca403fc625a5240b16c3fc246911
Wei Wang <wei@fastmail.com>
2015-12-31 12:59:34 -0500

    Add basePath option.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import path from 'path';
+
 export default function ({types: t}) {
+  const relativeOrAbsolutePath = new RegExp('^/|^./|^../');
+
   return {
     visitor: {
-      ImportDeclaration(path) {
+      ImportDeclaration(path, state) {
         const node = path.node;
         const importTarget = node.source.value;
 
@@ -19,11 +23,15 @@ export default function ({types: t}) {
 
         path.replaceWith(t.variableDeclaration('var', [
           t.variableDeclarator(varName, t.callExpression(readFileSync, [
-            t.stringLiteral(importTarget.split('!')[0]),
+            t.stringLiteral(resolveImportPath(importTarget.split('!')[0], state.opts.basePath)),
             t.stringLiteral('utf8')
           ]))
         ]));
       }
     }
   };
+
+  function resolveImportPath(target, base) {
+    return relativeOrAbsolutePath.test(target) ? target : path.join(base, target);
+  }
 }
---
+++ test/fixtures/from-base-path/expected.js
@@ -0,0 +1,1 @@
+var license = require('fs').readFileSync('/absolute/test/path/LICENSE', 'utf8');
